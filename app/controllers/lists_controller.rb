7800fe4acf6ecf0479724a7cd8b6c921890df3f1
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-09-04 03:16:15 +0000

    [finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,9 +24,14 @@ class ListsController < ApplicationController
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
-
+    redirect_to list_path(list.id)
   end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end 
   
   private
   
